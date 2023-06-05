6f59a80716479122539e4a3c9c3c72b244d4a3d2
TheFixon <135597755+TheFixon@users.noreply.github.com>
2023-06-05 14:34:06 +0500

    V2.0

---
+++ pingpong_main.py
@@ -0,0 +1,47 @@
+from pygame import *
+
+window_width = 600
+window_height = 500
+back = (200,255,255)
+window = display.set_mode(window_width, window_height)
+
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def updateL(self):
+        keys=key.get_pressed()
+        if keys[K_w] and self.rect.y>5:
+            self.rect.y-=self.speed
+        if keys[K_s] and self.rect.y<window_height-80:
+            self.rect.y+=self.speed
+
+    def updateR(self):
+        keys=key.get_pressed()
+        if keys[K_UP] and self.rect.y>5:
+            self.rect.y-=self.speed
+        if keys[K_DOWN] and self.rect.y<window_height-80:
+            self.rect.y+=self.speed
+
+game = True
+finish = False
+clock = time.Clock()
+fps = 60
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+        if finish != True:
+            window.fill(back)
+        clock.tick(fps)
+        display.update()
