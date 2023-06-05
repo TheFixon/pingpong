22f23e5701a33c230e40caac0074d5626100bc07
TheFixon <135597755+TheFixon@users.noreply.github.com>
2023-06-05 15:49:36 +0500

    V2.2

--- pingpong_main.py
+++ pingpong_main.py
@@ -3,7 +3,10 @@ from pygame import *
 window_width = 600
 window_height = 500
 back = (200,255,255)
-window = display.set_mode(window_width, window_height)
+window = display.set_mode((window_width, window_height))
+
+img_ball = "pingpong_ball.png"
+img_pong = "pingpong.png"
 
 
 class GameSprite(sprite.Sprite):
@@ -37,11 +40,39 @@ game = True
 finish = False
 clock = time.Clock()
 fps = 60
+racket1 = Player(img_pong, 30, 280, 50, 150, 4)
+racket2 = Player(img_pong, 520, 280, 50, 150, 4)
+ball = GameSprite(img_ball, 200, 200, 50, 50, 4)
+font.init()
+font1 = font.SysFont(None, 35)
+lose1=font1.render("Player 1 lose",True, (100, 0, 0))
+lose2=font1.render("Player 2 lose",True, (100, 0, 0))
+speed_x = 3
+speed_y = 3
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-        if finish != True:
-            window.fill(back)
-        clock.tick(fps)
-        display.update()
+    if finish != True:
+        window.fill(back)
+        
+    racket1.updateL()
+    racket2.updateR()
+    ball.rect.x+=speed_x
+    ball.rect.y+=speed_y
+    if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+        speed_x *= -1
+        speed_y *= 1
+    if ball.rect.y >= window_height or ball.rect.y <= 0:
+        speed_y *= -1
+    if ball.rect.x <= 0 or ball.rect.x >= window_width:
+        finish = True
+        if ball.rect.x <= 0:
+            window.blit(lose1, (200,200))
+        if ball.rect.x >= window_width:
+            window.blit(lose2, (200,200))
+    racket1.reset()
+    racket2.reset()
+    ball.reset()
+    clock.tick(fps)
+    display.update()
